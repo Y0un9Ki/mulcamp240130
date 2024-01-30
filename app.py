5368bd6f54a8b31a2af42f6a27392b0913d4dc09
Y0un9Ki <srilankakim66@gmail.com>
2024-01-30 18:24:18 +0900

    FIX : code FIX line38, line41

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 # @st.cache_data    
 # def load_data(loaddata):
@@ -10,7 +11,7 @@ import pandas as pd
 #     return df
 
 def main():
-    df1=pd.read_csv('data/products.csv')
+    df1=pd.read_csv('data/products(10000).csv')
     df2=pd.read_csv("data/departments.csv")
     st.title("물건의 위치는 밑에 표시됩니다.")
     with st.sidebar :
@@ -29,16 +30,15 @@ def main():
         find_id = st.selectbox("찾을 물건의 id",df1.loc[df1['department_id']==dpid, 'product_id'])
         find_product =  df1.loc[df1['product_id']==find_id, 'product_name']
         st.write("선택한 물건의 이름은 :", find_product)
-        
     # filter_pdid=df1.loc[df1['product_id']=='pdid', 'aisle_id']
     # filter_find_product=df1.loc[df1['product_name']=='find_product', 'aisle_id']
     # st.dataframe(filter_pdid)
     # st.dataframe(filter_find_product)
     st.write('id에 따른 물건의 복도 위치')
-    A=df1.loc[(df1['department_id']==dpid) , :]
+    A=df1.loc[(df1['department_id']==dpid)&(df1['product_id']==find_id) , :]
     st.dataframe(A)
     st.write('물건 이름에 따른 복도 위치')
-    B=df1.loc[(df1['department_id']==dpid) & (df1['product_name']==find_product), :]
+    B=df1.loc[(df1['department_id']==dpid)&(df1['product_name']==dpproduct), :]
     st.dataframe(B)
     # & (df1['product_id']==pdid)
  
