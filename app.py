4bd88f1a559cef5b11bf4182732d28c11a79ca85
Y0un9Ki <srilankakim66@gmail.com>
2024-01-30 11:25:49 +0900

    CREATED : new files

---
+++ app.py
@@ -0,0 +1,12 @@
+# _*_ coding:utf-8 _*_
+import streamlit as st
+
+def main():
+    st.title("Hellow World on Streamlit.io")
+    
+def load_data(df):
+    df=load_data("iris")
+    return df
+    
+if __name__ == "__main__":
+    main()
